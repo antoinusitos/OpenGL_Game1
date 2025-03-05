05035965504e9c0cd1eaee96a204e3f5090e0738
Antoine Gargasson <antoine.gargasson@starbreeze.com>
2025-03-05 12:54:56 +0100

    REworked cube to have it's own class

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,7 +1,7 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#define STB_IMAGE_IMPLEMENTATION
-#include "utilities/stb_image.h"
+/*#define STB_IMAGE_IMPLEMENTATION
+#include "utilities/stb_image.h"*/
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
@@ -9,6 +9,7 @@
 
 #include "utilities/shader_s.h"
 #include "Camera.h"
+#include "Cube.h"
 
 #include <iostream>
 
@@ -26,7 +27,7 @@ float deltaTime = 0.0f;	// Time between current frame and last frame
 float lastFrame = 0.0f; // Time of last frame
 
 // camera
-Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+Camera camera(glm::vec3(0.0f, 0.0f, 1.0f));
 float lastX = SCR_WIDTH / 2;
 float lastY = SCR_HEIGHT / 2;
 bool firstMouse = true;
@@ -73,140 +74,67 @@ int main()
 
 	// build and compile our shader zprogram
 	// ------------------------------------
-	Shader ourShader("src/3.3.coordinate_system.vs", "src/3.3.coordinate_system.fs");
-
-	// set up vertex data (and buffer(s)) and configure vertex attributes
-	// ------------------------------------------------------------------
-	float vertices[] = {
-	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-	 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
-	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-
-	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-	 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-	 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-	-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
-	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-
-	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-	-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-
-	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-	 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-	 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-	 0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-
-	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-	 0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
-	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-
-	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-	-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
-	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
-	};
+	Shader* ourShader = new Shader("src/3.3.coordinate_system.vs", "src/3.3.coordinate_system.fs");
 
 	glm::vec3 cubePositions[] = {
-		glm::vec3(0.0f,  0.0f,  0.0f),
-		glm::vec3(2.0f,  5.0f, -15.0f),
-		glm::vec3(-1.5f, -2.2f, -2.5f),
-		glm::vec3(-3.8f, -2.0f, -12.3f),
-		glm::vec3(2.4f, -0.4f, -3.5f),
-		glm::vec3(-1.7f,  3.0f, -7.5f),
-		glm::vec3(1.3f, -2.0f, -2.5f),
-		glm::vec3(1.5f,  2.0f, -2.5f),
-		glm::vec3(1.5f,  0.2f, -1.5f),
-		glm::vec3(-1.3f,  1.0f, -1.5f)
+	//dirt
+		glm::vec3(0.0f,  -1.50f,  0.0f),
+		glm::vec3(0.0f,  -1.50f,  -1.0f),
+		glm::vec3(0.0f,  -1.50f,  -2.0f),
+		glm::vec3(0.0f,  -1.50f,  -3.0f),
+		glm::vec3(0.0f,  -1.50f,  -4.0f),
+		glm::vec3(0.0f,  -1.50f,  -5.0f),
+		glm::vec3(0.0f,  -1.50f,  -6.0f),
+		glm::vec3(0.0f,  -1.50f,  -7.0f),
+		glm::vec3(0.0f,  -1.50f,  -8.0f),
+		glm::vec3(0.0f,  -1.50f,  -9.0f),
+		glm::vec3(0.0f,  -1.50f,  -10.0f),
 	};
 
-	unsigned int VBO, VAO;
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
+	Cube* cubes[] = {
+		//dirt
+		new Cube(glm::vec3(0.0f,  -1.50f,  0.0f), "Textures/dirt.png"),
+		new Cube(glm::vec3(0.0f,  -1.50f,  -1.0f), "Textures/dirt.png"),
+		new Cube(glm::vec3(0.0f,  -1.50f,  -2.0f), "Textures/dirt.png"),
+		new Cube(glm::vec3(0.0f,  -1.50f,  -3.0f), "Textures/dirt.png"),
+		new Cube(glm::vec3(0.0f,  -1.50f,  -4.0f), "Textures/dirt.png"),
+		new Cube(glm::vec3(0.0f,  -1.50f,  -5.0f), "Textures/dirt.png"),
+		new Cube(glm::vec3(0.0f,  -1.50f,  -6.0f), "Textures/dirt.png"),
+		new Cube(glm::vec3(0.0f,  -1.50f,  -7.0f), "Textures/dirt.png"),
+		new Cube(glm::vec3(0.0f,  -1.50f,  -8.0f), "Textures/dirt.png"),
+		new Cube(glm::vec3(0.0f,  -1.50f,  -9.0f), "Textures/dirt.png"),
+		new Cube(glm::vec3(0.0f,  -1.50f,  -10.0f), "Textures/dirt.png"),
 
-	glBindVertexArray(VAO);
+		//grass
+		new Cube(glm::vec3(-1.0f,  -1.50f,  0.0f), "Textures/grass.png"),
+		new Cube(glm::vec3(-1.0f,  -1.50f,  -1.0f), "Textures/grass.png"),
+		new Cube(glm::vec3(-1.0f,  -1.50f,  -2.0f), "Textures/grass.png"),
+		new Cube(glm::vec3(-1.0f,  -1.50f,  -3.0f), "Textures/grass.png"),
+		new Cube(glm::vec3(-1.0f,  -1.50f,  -4.0f), "Textures/grass.png"),
+		new Cube(glm::vec3(-1.0f,  -1.50f,  -5.0f), "Textures/grass.png"),
+		new Cube(glm::vec3(-1.0f,  -1.50f,  -6.0f), "Textures/grass.png"),
+		new Cube(glm::vec3(-1.0f,  -1.50f,  -7.0f), "Textures/grass.png"),
+		new Cube(glm::vec3(-1.0f,  -1.50f,  -8.0f), "Textures/grass.png"),
+		new Cube(glm::vec3(-1.0f,  -1.50f,  -9.0f), "Textures/grass.png"),
+		new Cube(glm::vec3(-1.0f,  -1.50f,  -10.0f), "Textures/grass.png"),
 
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	// position attribute
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-	// texture coord attribute
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-	glEnableVertexAttribArray(1);
-
-
-	// load and create a texture 
-	// -------------------------
-	unsigned int texture1, texture2;
-	// texture 1
-	// ---------
-	glGenTextures(1, &texture1);
-	glBindTexture(GL_TEXTURE_2D, texture1);
-	// set the texture wrapping parameters
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	// set texture filtering parameters
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	// load image, create texture and generate mipmaps
-	int width, height, nrChannels;
-	stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded texture's on the y-axis.
-	unsigned char* data = stbi_load("Textures/container.jpg", &width, &height, &nrChannels, 0);
-	if (data)
-	{
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-		glGenerateMipmap(GL_TEXTURE_2D);
-	}
-	else
-	{
-		std::cout << "Failed to load texture" << std::endl;
-	}
-	stbi_image_free(data);
-	// texture 2
-	// ---------
-	glGenTextures(1, &texture2);
-	glBindTexture(GL_TEXTURE_2D, texture2);
-	// set the texture wrapping parameters
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	// set texture filtering parameters
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	// load image, create texture and generate mipmaps
-	data = stbi_load("Textures/awesomeface.png", &width, &height, &nrChannels, 0);
-	if (data)
-	{
-		// note that the awesomeface.png has transparency and thus an alpha channel, so make sure to tell OpenGL the data type is of GL_RGBA
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-		glGenerateMipmap(GL_TEXTURE_2D);
-	}
-	else
-	{
-		std::cout << "Failed to load texture" << std::endl;
-	}
-	stbi_image_free(data);
+		new Cube(glm::vec3(1.0f,  -1.50f,  0.0f), "Textures/grass.png"),
+		new Cube(glm::vec3(1.0f,  -1.50f,  -1.0f), "Textures/grass.png"),
+		new Cube(glm::vec3(1.0f,  -1.50f,  -2.0f), "Textures/grass.png"),
+		new Cube(glm::vec3(1.0f,  -1.50f,  -3.0f), "Textures/grass.png"),
+		new Cube(glm::vec3(1.0f,  -1.50f,  -4.0f), "Textures/grass.png"),
+		new Cube(glm::vec3(1.0f,  -1.50f,  -5.0f), "Textures/grass.png"),
+		new Cube(glm::vec3(1.0f,  -1.50f,  -6.0f), "Textures/grass.png"),
+		new Cube(glm::vec3(1.0f,  -1.50f,  -7.0f), "Textures/grass.png"),
+		new Cube(glm::vec3(1.0f,  -1.50f,  -8.0f), "Textures/grass.png"),
+		new Cube(glm::vec3(1.0f,  -1.50f,  -9.0f), "Textures/grass.png"),
+		new Cube(glm::vec3(1.0f,  -1.50f,  -10.0f), "Textures/grass.png"),
+	};
 
 	// tell opengl for each sampler to which texture unit it belongs to (only has to be done once)
 	// -------------------------------------------------------------------------------------------
-	ourShader.use();
-	ourShader.setInt("texture1", 0);
-	ourShader.setInt("texture2", 1);
+	ourShader->use();
+	ourShader->setInt("texture1", 0);
 
 	// render loop
 	// -----------
@@ -225,35 +153,19 @@ int main()
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		// bind textures on corresponding texture units
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, texture1);
-		glActiveTexture(GL_TEXTURE1);
-		glBindTexture(GL_TEXTURE_2D, texture2);
-
 		// activate shader
-		ourShader.use();
+		ourShader->use();
 
 		// create transformations
 		// view = camera position
-		ourShader.setMat4("view", camera.GetViewMatrix());
+		ourShader->setMat4("view", camera.GetViewMatrix());
 		// projection = Camera info (fov, near, far)
 		glm::mat4 projection = glm::perspective(glm::radians(camera.Fov), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-		ourShader.setMat4("projection", projection);
+		ourShader->setMat4("projection", projection);
 
-		// render container
-		glBindVertexArray(VAO);
-		for (unsigned int i = 0; i < 10; i++)
+		for (unsigned int i = 0; i < sizeof(cubes) / sizeof(cubes[0]); i++)
 		{
-			// calculate the model matrix for each object and pass it to shader before drawing
-			// model = object info (pos, angle, etc.)
-			glm::mat4 model = glm::mat4(1.0f);
-			model = glm::translate(model, cubePositions[i]);
-			float angle = 20.0f * i;
-			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
-			ourShader.setMat4("model", model);
-
-			glDrawArrays(GL_TRIANGLES, 0, 36);
+			cubes[i]->Render(ourShader);
 		}
 
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
@@ -264,8 +176,12 @@ int main()
 
 	// optional: de-allocate all resources once they've outlived their purpose:
 	// ------------------------------------------------------------------------
-	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VBO);
+	for (unsigned int i = 0; i < 10; i++)
+	{
+		cubes[i]->Free();
+	}
+
+	delete ourShader;
 
 	// glfw: terminate, clearing all previously allocated GLFW resources.
 	// ------------------------------------------------------------------
