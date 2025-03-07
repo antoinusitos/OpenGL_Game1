9db6f64c07b1b9e95940f1223574a1fd08c307dc
antoinusitos <antoine.gargasson@gmail.com>
2025-03-07 23:06:09 +0100

    Update ResourceLoader.h

--- src/ResourceLoader.h
+++ src/ResourceLoader.h
@@ -8,6 +8,7 @@ struct Texture2D
     int width;
     int height;
     int nrChannels;
+    unsigned int* id;
 };
 
 class ResourceLoader
@@ -21,7 +22,7 @@ public:
     }
 
 public:
-	void GetTexture(const char* texture, int& inWidth, int& inHeight, int& inNrChannels);
+    unsigned int* GetTexture(const char* texture, int& inWidth, int& inHeight, int& inNrChannels);
 
     void Free();
 
